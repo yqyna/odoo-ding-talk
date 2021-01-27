11e81dc148ce8a5221da863e50c91c294511f7ea
“yuhaiping <y960754862@163.com>
2021-01-27 15:29:55 +0800

    钉钉--设置钉钉组管理员权限

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo钉钉",
+    'summary': """Odoo钉钉模块""",
+    'description': """Odoo钉钉模块""",
+    'category': 'dingtalk',
+    'version': '12.1.2',
+    'depends': ['base', 'hr', 'mail', 'auth_oauth', 'hr_attendance'],
+    'external_dependencies': {
+        'python': ['pypinyin', 'pycryptodome', 'dingtalk-sdk'],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'data': [
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'views/dingtalk_menus.xml',
+        'views/dingtalk_config.xml',
+    ],
+    'qweb': [
+        'static/xml/*.xml',
+    ],
+    'images': [
+        'static/description/icon.png',
+    ],
+    'license': 'AGPL-3',
+}
