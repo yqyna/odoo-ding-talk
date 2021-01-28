eb27041c9d80f72f0f0654e5fc563994d0f2fb21
“yuhaiping <y960754862@163.com>
2021-01-28 15:47:17 +0800

    钉钉--钉钉创建odoo系统用户发送通知信息

--- models/res_users.py
+++ models/res_users.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import threading
 from odoo import api, fields, models, SUPERUSER_ID
 
 
@@ -11,3 +12,21 @@ class ResUsers(models.Model):
 
     ding_user_phone = fields.Char(string='钉钉登录手机', index=True)
     ding_user_id = fields.Char(string='钉钉访问令牌', index=True)
+
+    @api.model
+    def create(self, values):
+        user = super(ResUsers, self).create(values)
+        config = self.env['dingtalk.message.config'].is_new_user_send_msg()
+        if config and user.employee:
+            employee = None
+            for emp in user.employee_ids:
+                if emp.ding_id:
+                    employee = emp
+                    break
+            if employee:
+                msg_body = config.msg_body
+                ding_id = employee.ding_id
+                message_tool = self.env['dingtalk.message.tool']
+                threading.Thread(target=message_tool.send_create_user_message,
+                                 args=(employee.company_id, ding_id, msg_body, user.id)).start()
+        return user
